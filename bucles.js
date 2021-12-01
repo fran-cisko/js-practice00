f25107791f2c8b7f221a70f689bf019eeb84b68b
fran-cisko <franciscosciarretta@gmail.com>
2021-12-01 18:18:13 -0300

    estructuras de control

---
+++ bucles.js
@@ -0,0 +1,9 @@
+document.write('secuencia de números hecha con do while');
+let number = -5;
+
+do{
+    document.write(number+"<br>");
+    number++;
+}
+while(number <= 6);
+
---
+++ while.js
@@ -0,0 +1,9 @@
+document.write('<br><br>secuencia de números hecha con while');
+
+number= -3;
+
+while(number <= 6) {
+  
+    number++;
+    document.write(number+"<br>");
+}
